4343463d7a0488bf36d8caaef3aacbaaa5dd3d3e
Ahmed Kamel <ahmedkamel89@gmail.com>
2020-04-02 00:11:07 +0200

    Fix broken lines

--- App/src/config.js
+++ App/src/config.js
@@ -4,7 +4,7 @@
 
 let config = {
 
-        token : "BQArw1Q6L9NFH8Z0bMzo8W1eai_owOTcY9AQ9Kiz53Fb8ESe3AqomrSjutJXpT-X5KCXqCHJJ2zUiiluW6pxFYVys5__lExMFamaoMplGrkuP_-fwadAEwHf3TlXHp0mQSN9_5A_qKlTZvhXACF9-EDGWQQcKjBFBOsu3ShenqNUM3Sv",
+        token : "QArw1Q6L9NFH8Z0bMzo8W1eai_owOTcY9AQ9Kiz53Fb8ESe3AqomrSjutJXpT-X5KCXqCHJJ2zUiiluW6pxFYVys5__lExMFamaoMplGrkuP_-fwadAEwHf3TlXHp0mQSN9_5A_qKlTZvhXACF9-EDGWQQcKjBFBOsu3ShenqNUM3Sv",
             
         isLoading : false,
        
